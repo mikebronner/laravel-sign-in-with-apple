11eea8ee85254e64d17303bfe8206f264e8c5702
Mike Bronner <mike@genealabs.com>
2019-10-12 10:52:34 -0700

    Fix env variable in config.

--- config/services.php
+++ config/services.php
@@ -2,7 +2,7 @@
 
 return [
     "sign_in_with_apple" => [
-        "login" => env("SIGN_IN_WITH_APPLE_REDIRECT"),
+        "login" => env("SIGN_IN_WITH_APPLE_LOGIN"),
         "redirect" => env("SIGN_IN_WITH_APPLE_REDIRECT"),
         "client_id" => env("SIGN_IN_WITH_APPLE_CLIENT_ID"),
         "client_secret" => env("SIGN_IN_WITH_APPLE_CLIENT_SECRET"),
