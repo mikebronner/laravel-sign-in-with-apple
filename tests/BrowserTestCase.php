8dab019c323ad164a21c37fc88edc3f0aa3cda4b
Mike Bronner <mike@genealabs.com>
2020-09-13 22:39:34 +0000

    WIP

--- tests/BrowserTestCase.php
+++ tests/BrowserTestCase.php
@@ -7,7 +7,4 @@ use Orchestra\Testbench\Dusk\TestCase;
 abstract class BrowserTestCase extends TestCase
 {
     use CreatesApplication;
-
-    protected static $baseServeHost = '127.0.0.1';
-    protected static $baseServePort = 9000;
 }
